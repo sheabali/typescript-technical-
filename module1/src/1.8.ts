ffdd6af074062f6a3d00973a550683607af6a847
Pranto Sheikh <prantosheikh834@gmail.com>
2024-11-02 22:01:44 +0600

    1-9 Type alias in typescript

---
+++ module1/src/1.8.ts
@@ -0,0 +1,27 @@
+{
+
+const user = {
+    id: 412,
+    name:{
+        firstName: "sheikh",
+        middleName: "pranto",
+        lastName:"sk",
+        
+    },
+    contactNo: "01701111111",
+        address:"BD"
+}
+
+const {contactNo} = user
+
+
+
+
+
+
+
+
+
+
+
+}
