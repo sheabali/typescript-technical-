5e443968ff90279e5d1f8a90b89ad3e741dbb496
Sheab Ali <sheabali@SAKIBs-MacBook-Pro.local>
2024-11-08 15:09:05 +0600

    location

---
+++ module2/2.1.ts
@@ -0,0 +1,25 @@
+{
+//
+    // type assertion
+let anyithig : any;
+
+anyithig  = "Next leve";
+
+
+(anyithig as string)
+
+anyithig = 222;
+
+const kgTogm = (value:string | number) => {
+
+if(value === 'string'){
+    const convertedValue = parseFloat(value)*(1000)
+    return convertedValue
+}
+
+}
+
+
+//
+
+}
