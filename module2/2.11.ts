fcb475a550686bd3b102d4bc2c985f080678e008
Sheab Ali <sheabali@SAKIBs-MacBook-Pro.local>
2024-11-11 01:08:25 +0600

    Utility types finish

--- module2/2.11.ts
+++ module2/2.11.ts
@@ -1,5 +1,53 @@
 {
-    // 
+    //  utility types
 
-    
+type Person = {
+    name: string,
+    age: number,
+    email?: string,
+    contactNo: string
+}
+
+// Pick
+type NameAge = Pick<Person, "name"|"age">
+
+// Omit
+type ContctInfo = Omit<Person, "name"| "age">
+
+// Required 
+type PersonRequired = Required<Person>
+
+// Partial 
+type PersonPartial = Partial<Person>
+
+// ReadOnly 
+const person1 : Person = {
+name: "Mr. S",
+age: 32,
+contactNo: "015"
+}
+
+person1.name = "Mr. P"
+
+// Record
+// type MyObj = {
+//     a:string,
+//     b:string
+// }
+type MyObj = Record<string, number>
+
+const EmptyObj : Record<string,unknown> = {}
+
+const obj1: MyObj = {
+    a:"aa",
+    b:'b',
+    c:"cc",
+    d:4
+}
+
+
+
+
+
+    // 
 }
