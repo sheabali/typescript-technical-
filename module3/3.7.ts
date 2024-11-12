284f1dfd7d9d14f200c101572ffa3216b4b47f5d
Sheab Ali <sheabali@SAKIBs-MacBook-Pro.local>
2024-11-12 19:29:41 +0600

    3-7 Statics in OOP

--- module3/3.7.ts
+++ module3/3.7.ts
@@ -1,7 +1,24 @@
 {
-    // 
+    // static
 
+    class Counter {
+       static count : number = 0;
+       static increment(){
+            return (Counter.count = Counter.count + 1)
+        }
 
+      static  decrement(){
+            return (Counter.count = Counter.count - 1)
+        }
+    }
+
+    // const instance1 = new Counter()
+    console.log(Counter.increment()) // difference memory
+   
+    // const instance2 = new Counter()
+    console.log(Counter.increment()) // difference memory
+    console.log(Counter.increment()) // difference memory
+    
 
     // 
 }
