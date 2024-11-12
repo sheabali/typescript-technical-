6704b355cc3a3a71f841454f06ef69903aca0e19
Sheab Ali <sheabali@SAKIBs-MacBook-Pro.local>
2024-11-12 17:30:16 +0600

    ab

---
+++ module3/3.4.ts
@@ -0,0 +1,58 @@
+{
+    //
+
+    //instanceof guard
+
+    class Animal {
+        name: string;
+        species: string
+
+        constructor(name: string, species:string){
+            this.name =name,
+            this.species = species
+        }
+
+        makeSound() {
+            console.log('I am making sound')
+        }
+    }
+
+    class Dog extends Animal{
+        constructor(name: string, species: string){
+            super(name,species)
+        }
+
+        makeBark(){
+            console.log('I am barking')
+        }
+    }
+
+    class Cat extends Animal{
+        constructor(name: string, species: string){
+            super(name,species)
+        }
+
+        makeMeaw(){
+            console.log('I am Meawing')
+        }
+    }
+
+    const getAnimal = (animal: Animal) => {
+        if(animal instanceof Dog){
+        animal.makeBark()
+    }
+    else if(animal instanceof Cat){
+        animal.makeMeaw()
+    }else{
+        animal.makeSound()
+    }
+    }
+
+
+    const dog = new Dog("Dog Bhai", "Dog")
+    const cat = new Cat("Cat Bhai", "Cat")
+
+
+
+    //
+}
