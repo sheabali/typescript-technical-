bea39037c269d9e0c2c0df24f13b6724845a63c3
Sheab Ali <sheabali@SAKIBs-MacBook-Pro.local>
2024-11-13 17:03:52 +0600

    finish

--- module3/3.8.ts
+++ module3/3.8.ts
@@ -1,7 +1,80 @@
 {
-    //
+    // polymorphiam
+
+    class Person {
+        getSleep(){
+            console.log(`I am sleeping for 8 hours per day`)
+        }
+    }
+
+    class Student extends Person {
+        getSleep(){
+            console.log(`I am sleeping for 7 hours per day`)
+        }
+    }
+
+    class Developer extends Person {
+        getSleep(){
+            console.log(`I am sleeping for 6 hours per day`)
+        }
+    }
+
+    class Shape {
+        getArea() : number {
+            return 0
+        }
+    }
+
+    //  pi* r* r 
+    class Circle extends Shape {
+        radius : number;
+        constructor(radius: number){
+            super()
+            this.radius = radius
+        }
+        getArea() : number {
+            return Math.PI * this.radius * this.radius;
+        }
+    }
+
+    // height width
+
+    class Rectangle extends Shape {
+        height : number;
+        width : number;
+        constructor(height: number, width: number){
+            super()
+            this.height = height,
+            this.width = width
+        }
+        getArea() : number {
+            return this.height * this.width
+        }
+    }
+
+    const getShapeArea = (param: Shape) => {
+        console.log(param.getArea())
+    }
+
+    const shape1 = new Shape()
+    const shape2 = new Circle(12)
+    const shape3 = new Rectangle(12,43)
+
+    getShapeArea(shape1)
 
 
+
+    const getSleepingHours = (param : Person) => {
+        param.getSleep()
+    }
+
+    const person1 = new Person()
+    const person2 = new Student()
+    const person3 = new Developer()
+
+    getSleepingHours(person1)
+    getSleepingHours(person2)
+    getSleepingHours(person3)
 
     // 
 }
